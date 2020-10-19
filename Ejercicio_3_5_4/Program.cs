efc76ef40b7fa353b56c2a56c410c4747c4cd50f
viworh <gusiluzman777.omg@gmail.com>
2020-10-19 12:44:35 +0200

    Agregar archivos de proyecto.

---
+++ Ejercicio_3_5_4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ejercicio_3_5_4
+{
+    class Program
+    {
+        static int n1, n2;
+        static bool ambosPares;
+        static void Main(string[] args)
+        {
+            n1 = Convert.ToInt32(Console.ReadLine());
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            ambosPares = (n1 % 2 == 0) && (n2 % 2 == 0);
+
+            if(ambosPares)
+            {
+                Console.WriteLine(ambosPares);
+            }
+            else
+            {
+                Console.WriteLine(ambosPares);
+            }
+            
+        }
+    }
+}
